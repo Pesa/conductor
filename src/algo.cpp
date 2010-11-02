2fc8a37c0061f4a824d9d0c488b535dc1f84d76e
Davide Pesavento <davidepesa@gmail.com>
2010-11-02 01:27:20 +0100

    algo: simplify and cleanup the code.

--- src/algo.cpp
+++ src/algo.cpp
@@ -1,59 +1,59 @@
+#include <limits>
+#include <QHash>
 #include <QList>
 #include <QPair>
-#include <QString>
 #include <QMap>
-#include <QMapIterator>
 #include <QtDebug>
 
-using namespace std;
+#include "probemanager.h"
 
-static int compare(const QPair<QString, double> a, const QPair<QString, double> b){
+#define NULL_RSSI -999
 
-    return ( a.second - b.second );
+
+static int compare(const QPair<QString, double> a, const QPair<QString, double> b)
+{
+    return a.second - b.second;
 }
 
-static QString calculate_current_room(QMap<QString, double> m){
+static QString calculate_current_room(QHash<QString, int> map)
+{
+    int maxRssi = std::numeric_limits<int>::min();
+    QString room;
 
-    QString index_current_room = "UNDEF";
-    //initialization max RSSI
-    double max = -100000000;
-    QMapIterator<QString, double> iterator(m);
-
-    while(iterator.hasNext()){
-        iterator.next();
-
-        if ( ((iterator.value()) != -999) and ((iterator.value()) >= max) )
-        {
-            max = iterator.value();
-            index_current_room = iterator.key();
+    QHashIterator<QString, int> i(map);
+    while (i.hasNext()) {
+        i.next();
+        if (i.value() != NULL_RSSI && i.value() > maxRssi) {
+            maxRssi = i.value();
+            room = i.key();
         }
     }
-    qDebug() << "Current_room: " << index_current_room;
-    return index_current_room;
 
+    return room;
 }
 
 //return the list of host indexes no null
-static QList<QString> get_index_no_null_RSSI(QMap<QString, double> m){
+static QList<QString> get_index_no_null_RSSI(QHash<QString, int> m)
+{
     QList<QString> list;
-    QMapIterator<QString, double> iterator(m);
-    while(iterator.hasNext()){
+    QHashIterator<QString, int> iterator(m);
+    while(iterator.hasNext()) {
         iterator.next();
-        if(iterator.value() != -999){
+        if(iterator.value() != NULL_RSSI){
             list.append(iterator.key());
         }
     }
     return list;
-
 }
 
 //implements the AND operation between the list of indexes available and their neighbours
-static QList<QString> get_intersected_neighbours(QList<QString> indexes, QMap<QString, QList<QString> > adj, QString room){
+static QList<QString> get_intersected_neighbours(QList<QString> indexes, QMap<QString, QList<QString> > adj, QString room)
+{
     QList<QString> results;
-    QMap<QString, QList<QString> >::iterator i = adj.begin();
-    i = adj.find(room);
-    QList<QString> neighbours = i.value();
-    for(int i = 0; i < indexes.count(); i++ ){
+
+    QList<QString> neighbours = adj.value(room);
+
+    for (int i = 0; i < indexes.count(); i++) {
         bool found = false;
         for(int j = 0; j < neighbours.count() and !found; j++){
             if(indexes.at(i) == neighbours.at(j)){
@@ -66,13 +66,17 @@ static QList<QString> get_intersected_neighbours(QList<QString> indexes, QMap<QS
 }
 
 //initial map, a list of indexes with no null RSSI, current room
-static QList<QString> get_sink_indexes(QMap<QString, double> map, QList<QString> l, int max_number, QString room, QMap<QString, QList<QString> > adj)
+static QList<QString> get_sink_indexes(QHash<QString, int> map, // RSSI attuali
+                                       QList<QString> l, // lista stanze con RSSI non nullo
+                                       int max_number, // n. max di diffusori contemporanei
+                                       QString room, // stanza attuale
+                                       QMap<QString, QList<QString> > adj) // adiacenza stanze
 {
     QList<QString> results;
     int size = l.count();
-    QMap<QString, double>::iterator i = map.begin();
-    switch (size) {
+    QHash<QString, int>::iterator i = map.begin();
 
+    switch (size) {
     case 0:
         // TODO: pause
         break;
@@ -89,14 +93,14 @@ static QList<QString> get_sink_indexes(QMap<QString, double> map, QList<QString>
             QList<QPair<QString, double> > pair_list;
             for(int j = 0; j < l.count(); j++){
                 i = map.find(l.at(j));
-                double RSSI = i.value();
-                QPair<QString, double> pair(l.at(j), RSSI);
+                double rssi = i.value();
+                QPair<QString, double> pair(l.at(j), rssi);
                 pair_list.append(pair);
             }
             qSort(pair_list.begin(), pair_list.end(), compare);
 
             for(int k = 0; k < max_number; k++){
-                const QString id = (pair_list.at(k)).first;
+                QString id = (pair_list.at(k)).first;
                 restricted_list.append(id);
             }
         }
@@ -109,18 +113,14 @@ static QList<QString> get_sink_indexes(QMap<QString, double> map, QList<QString>
 
 void algo_test()
 {
-    //In map keys = index hosts, double values = RSSI
-    QMap<QString, double> map;
+    ProbeManager man;
 
-    map["1"] = -0.0001;
-    map["2"] = 0; //It should be the current room
-    map["3"] = -999; //-999 is fixed(by MARTY) value for NULL value
-    map["4"] = -5.98;
-    map["5"] = -0.23;
+    // map: room name => RSSI
+    QHash<QString, int> map = man.rssiMap("00:23:D6:9A:DC:25");
 
-
-    //In map keys = index hosts, a list of adjacent hosts
+    // map: room name => list of adjacent rooms (maybe use a QSet?)
     QMap<QString, QList<QString> > adjacencies;
+
     QList<QString> list1;
     list1 << "3" << "4";
     adjacencies["1"] = list1;
@@ -141,29 +141,20 @@ void algo_test()
     list5 << "2" << "4";
     adjacencies["5"] = list5;
 
-    //MIN number RSSI necessary to proceed to choice
-    int min_number_RSSI = 3;
+    // min number RSSI necessary to proceed to choice
+    const int min_number_RSSI = 3;
 
-    //MAX number speakers activated simultaneously
-    int max_number_speakers_activated = 2;
+    // max number speakers activated simultaneously
+    const int max_number_speakers_activated = 2;
 
     QString current_room = calculate_current_room(map);
 
-    QList<QString> list_indexes_available = get_index_no_null_RSSI(map);
-
-    int number_RSSI_available = list_indexes_available.count();
-
-    //check if there are available data
-    if(number_RSSI_available < min_number_RSSI){
-        qDebug()<< "IT's necessary TO WAIT";
+    QList<QString> available = get_index_no_null_RSSI(map);
+    if (available.count() < min_number_RSSI) {
+        qDebug()<< "It's necessary TO WAIT";
         return;
     }
 
-    QList<QString> sink_indexes = get_sink_indexes(map, list_indexes_available, max_number_speakers_activated, current_room, adjacencies);
-
-    qDebug()<< "List sink indexes to use for playback:";
-    for(int i = 0; i< sink_indexes.count(); i++)
-    {
-        qDebug() << sink_indexes.at(i);
-    }
+    QList<QString> sinks = get_sink_indexes(map, available, max_number_speakers_activated, current_room, adjacencies);
+    qDebug() << "List sink indexes to use for playback:" << sinks;
 }
