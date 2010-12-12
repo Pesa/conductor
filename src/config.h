2575e59fc861cde31ff6a0e4c5fa296d856a6e67
Davide Pesavento <davidepesa@gmail.com>
2010-12-12 17:28:28 +0100

    Config: added updateInterval().

--- src/config.h
+++ src/config.h
@@ -10,11 +10,12 @@ class Config
 {
 public:
     static int maxRetries() { return 2; }
-    static int maxSimultaneousSpeakers() { return 3; }
+    static int maxSimultaneousSpeakers() { return 2; }
     static QHash<QString, QString> probesAddresses();
     static QList<QString> roomsNames();
     static QHash<QString, QSet<QString> > roomsTopology();
     static int rpcPort() { return 1234; }
+    static int updateInterval() { return 3000; }
 
 private:
     Config() {}
